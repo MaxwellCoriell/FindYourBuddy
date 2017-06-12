26033773cf7d781178cc2f45effd7c49d900e3dd
Maxwell C Baldridge <max.baldridge@gmail.com>
2017-06-12 15:06:59 -0500

    here's literally the most basic file structure ever for django

---
+++ fyb_API/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class FybApiConfig(AppConfig):
+    name = 'fyb_API'
---
+++ fyb_web/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class FybWebConfig(AppConfig):
+    name = 'fyb_web'

4c78d79120f860efb48cff2fc054357f01bf0e51
Maxwell C Baldridge <max.baldridge@gmail.com>
2017-06-19 12:43:18 -0500

    more refactoring

--- fyb_web/models.py
+++ fyb_web/models.py
@@ -1,47 +1,4 @@
-from django.contrib.auth.models import User
 from django.db import models
 
 
 # Create your models here.
-class LostPet(models.Model):
-    owner = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-    )
-    name = models.CharField(max_length=255)
-    species = models.CharField(
-        max_length=255
-    )
-    breed = models.CharField(
-        max_length=255
-    )
-    description = models.TextField()
-    last_seen_location = models.CharField(
-        max_length=255
-    )
-    reward = models.DecimalField(
-        blank=True,
-        null=True,
-        max_digits=6,
-        decimal_places=10,
-    )
-
-
-class FoundPet(models.Model):
-    finder = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-    )
-    name = models.CharField(max_length=255)
-    species = models.CharField(
-        max_length=255,
-    )
-    breed = models.CharField(
-        blank=True,
-        null=True,
-        max_length=255,
-    )
-    description = models.TextField()
-    last_seen_location = models.CharField(
-        max_length=255,
-    )
